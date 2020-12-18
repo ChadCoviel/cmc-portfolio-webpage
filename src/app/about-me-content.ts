04d28885de8d032e1e23a10b5beae48d364399af
Chad Coviel <covielc@gmail.com>
2020-12-18 01:51:51 -0500

    Changed appearance, added some icons, enabled smooth scrolling to content from side bar nav

--- src/app/about-me-content.ts
+++ src/app/about-me-content.ts
@@ -2,21 +2,25 @@ export const about_me_content = [
   {
     name: 'Education',
     description: 'educational background',
+    id: 'edu-content',
     required: true
   },
   {
-    name: 'Professional Career',
-    description: 'professional career progress',
+    name: 'Languages and Technologies',
+    description: 'personal and professional goals',
+    id: 'tech-content',
     required: true
   },
   {
     name: 'Goals',
     description: 'personal and professional goals',
+    id: 'goals-content',
     required: true
   },
   {
     name: 'Interests',
     description: 'interests outside of work',
+    id: 'interests-content',
     required: true
   }
 ];
--- src/app/links.ts
+++ src/app/links.ts
@@ -3,12 +3,14 @@ export const links = [
     name: 'GitHub',
     description: 'My GitHub',
     value: 'https://github.com/ChadCoviel',
+    image: '../../assets/img/github.svg',
     path: 'AboutMe'
   },
   {
     name: 'LinkedIn',
     description: 'My LinkedIn profile',
     value: 'https://www.linkedin.com/in/chad-coviel-1516a054/',
+    image: '../../assets/img/linkedin.svg',
     path: 'links'
   }
 ];
